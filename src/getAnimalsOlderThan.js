761b796d7e8e0a9c0eae93eb4aa6d9163c74376d
Jean Viegas <jeanmelg@gmail.com>
2021-11-24 17:20:02 -0300

    finaliza getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specie = species.find((specie) => specie.name === animal);
+  const residents = specie.residents;
+  const residentsWithAgeUpThan = residents.every((res) => res.age >= age);
+  return residentsWithAgeUpThan;
 }
 
 module.exports = getAnimalsOlderThan;
