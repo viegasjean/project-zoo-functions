1792a33e56d167255b20f26b95832f6cb55f448e
Jean Viegas <jeanmelg@gmail.com>
2021-11-25 18:36:52 -0300

    finaliza função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  const animalSpecie = species.find((specie) => animal.specie === specie.name);
+  const { residents } = animalSpecie;
+  const animalSex = residents.filter((resident) => resident.sex === animal.sex);
+  if (animal.specie && animal.sex) {
+    return animalSex.length;
+  }
+  if (animal.specie) {
+    return animalSpecie.residents.length;
+  }
 }
 
 module.exports = countAnimals;
