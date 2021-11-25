7021924f69e3f36da18ca748fbe7b531887a5331
Jean Viegas <jeanmelg@gmail.com>
2021-11-25 00:22:16 -0300

    formata codigo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,8 +4,7 @@ const data = require('../data/zoo_data');
 function isManager(id) {
   // seu código aqui
 
-  return employees.some(employe => employe.managers.includes(id))
-
+  return employees.some((employe) => employe.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
@@ -13,17 +12,17 @@ function getRelatedEmployees(managerId) {
   /* const managedEmployees =  employees.filter(employe => {
     return employe.managers.includes(managerId)
   }) */
-  if (!isManager(managerId)){
+  if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const managedEmployees =  employees.reduce((accManagedEmployees, employe) => {
-    if (employe.managers.includes(managerId)){
-      accManagedEmployees.push(`${employe.firstName} ${employe.lastName}`)
+  const managedEmployees = employees.reduce((accManagedEmployees, employe) => {
+    if (employe.managers.includes(managerId)) {
+      accManagedEmployees.push(`${employe.firstName} ${employe.lastName}`);
     }
-    return accManagedEmployees
-  }, [])
+    return accManagedEmployees;
+  }, []);
 
-  return managedEmployees
+  return managedEmployees;
 }
 
 module.exports = { isManager, getRelatedEmployees };
