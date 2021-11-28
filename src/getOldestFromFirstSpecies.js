260e6b4cb3b5bbbd148eda311f4aed927f71e3b0
Jean Viegas <jeanmelg@gmail.com>
2021-11-27 23:48:11 -0300

    finaliza getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employe = employees.find(employe => employe.id === id) // Encontra o empregado com a id
+  const specie = species.find(specie => specie.id === employe.responsibleFor[0]) // Encontra a primeira id da especie que o empregado é responsavel
+  specie.residents.sort((a, b) => (a.age < b.age ? 1 : -1)) // Ordena os residents pela idade, do maior para o menor
+  const oldestResident = Object.values(specie.residents[0]) // armazena os values do resident mais velho que agora está na primeira posição
+  return oldestResident
+
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
-
+const { hours, species } = require('../data/zoo_data');
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const days = Object.entries(hours)
+  result = {}
+  days.forEach( day => {
+    speciesExibition = []
+    species.forEach(specie => {
+      if (specie.availability.includes(day[0])){
+        speciesExibition.push(specie.name)
+      }
+    })
+
+    if(day[1].open === 0){
+      result[day[0]] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!'
+      };
+    } else {
+      result[day[0]] = {
+        officeHour: `Open from ${day[1].open}am until ${day[1].close}pm`,
+        exhibition: speciesExibition
+      };
+    }
+  })
+  console.log(result[scheduleTarget])
+  return result
 }
+getSchedule('Monday')
 
 module.exports = getSchedule;
